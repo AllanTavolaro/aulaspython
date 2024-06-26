e1dbbfab66ecc6f640bed3e53d2b636f5f1907e6
allan tavolaro <allantavolaro@hotmail.com>
2024-06-26 20:20:49 -0300

    desconto para paula

---
+++ aula1/aula1.py
@@ -0,0 +1,15 @@
+"""
+O aluno Allan teve a primeira nota 2, a segunda nota 3, e a nota final 5
+
+"""
+
+nome = "Paula Martins"
+mes = "Janeiro"
+valor_compra = 5
+desconto = 10
+cupom = "PAULAÉ10"
+
+
+mensagem = f"O nome da pessoa é {nome}, desconto dela é {desconto}"
+
+print(mensagem)
