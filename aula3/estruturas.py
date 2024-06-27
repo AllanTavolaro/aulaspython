83a38d0fed37e187ff4cb7e66be026cce47de2bb
allan tavolaro <allantavolaro@hotmail.com>
2024-06-26 21:06:39 -0300

    times

---
+++ aula3/estruturas.py
@@ -0,0 +1,11 @@
+time = input("Digite seu time: ")
+print(time == 'Flamengo')
+
+if time == 'Flamengo':
+    print("Você é um Timão!")
+elif time == "Bahia":
+    print("Você é do Esquadrão")
+elif time == "Gremio":
+    print("Você é do Imortal")
+else:
+    print("Você não é um Timão!")
